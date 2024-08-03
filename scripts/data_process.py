17ecea52c7739a50e8b4a2fdfeeebfe92b179514
Raymond Guo <raymond.guo23@imperial.ac.uk>
2024-08-03 22:01:36 +0100

    Load into train, val, test datasets

--- scripts/data_process.py
+++ scripts/data_process.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import random
 
 
 def filter_for_npy(source_partial_dir_path, target_partial_dir_path, source_gt_dir_path, target_gt_dir_path):
@@ -32,10 +33,49 @@ def filter_for_npy(source_partial_dir_path, target_partial_dir_path, source_gt_d
             print(f"Copied {src} to {dest}")  
 
 
-if __name__ == '__main__':
-    source_partial_dir_path = '~/../../vol/bitbucket/rqg23/preprocessed_obj_partial_inputs'
-    target_partial_dir_path = '~/../../vol/bitbucket/rqg23/processed_pairs_data/partial'
-    source_gt_dir_path = '~/../../vol/bitbucket/rqg23/ground_truth_obj_processed'
-    target_gt_dir_path = '~/../../vol/bitbucket/rqg23/processed_pairs_data/ground_truth'
-    filter_for_npy(source_partial_dir_path, target_partial_dir_path, source_gt_dir_path, target_gt_dir_path)
+# Going to be 80% train, 10% val, 10% test
+def train_val_test_split(unsplit_directory_path, split_destination_path):
+    unsplit_directory_path = os.path.expanduser(unsplit_directory_path)
+    split_destination_path = os.path.expanduser(split_destination_path)
 
+    os.makedirs(split_destination_path, exist_ok = True)
+    unsplit_partial_data = os.path.join(unsplit_directory_path, 'partial')
+    unsplit_gt_data = os.path.join(unsplit_directory_path, 'ground_truth')
+
+    partial_inputs = [file for file in os.listdir(unsplit_partial_data)]
+    matched_files = [(file, file.replace('.npy', '_gt.npy')) for file in partial_inputs if os.path.exists(os.path.join(unsplit_gt_data, file.replace('.npy', '_gt.npy')))]
+    
+    random.shuffle(matched_files)
+    num_files = len(matched_files)
+    train_range = int(num_files * 0.8)
+    validation_range = train_range + int(num_files * 0.1)
+
+    train_idx = matched_files[:train_range]
+    val_idx = matched_files[train_range:validation_range]
+    test_idx = matched_files[validation_range:]
+
+    def assign_to_trainValTest(pairs, mode):
+        partial_inputs = os.path.join(split_destination_path, mode, 'partial_inputs')
+        ground_truths = os.path.join(split_destination_path, mode, 'ground_truths')
+
+        for partial, gt in pairs:
+            src_partial = os.path.join(unsplit_partial_data, partial)
+            dest_partial = os.path.join(partial_inputs, partial)
+            shutil.copy(src_partial, dest_partial)
+            print(f"Copied {src_partial} to {dest_partial}")
+
+            src_gt = os.path.join(unsplit_gt_data, gt)
+            dest_gt = os.path.join(ground_truths, gt)
+            shutil.copy(src_gt, dest_gt)
+            print(f"Copied {src_gt} to {dest_gt}")
+
+    assign_to_trainValTest(train_idx, 'train')
+    assign_to_trainValTest(val_idx, 'val')
+    assign_to_trainValTest(test_idx, 'test')
+
+
+
+if __name__ == '__main__':
+    unsplit_directory_path = '~/../../vol/bitbucket/rqg23/processed_pairs_data'
+    split_destination_path = '~/../../vol/bitbucket/rqg23/project_data'
+    train_val_test_split(unsplit_directory_path, split_destination_path)
